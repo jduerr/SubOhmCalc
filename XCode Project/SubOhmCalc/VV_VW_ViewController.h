9b0d7c7157df0d69dfa8394cdb89dc851502e293
Johannes Dürr <mail@johannes-duerr.de>
2014-11-18 10:15:24 +0100

    Added Variable Voltage / Variable Wattage Calc
    Added implementation for the VV / VW calculation to VVVWViewController.
    Added needed Controls and labels to Storyboard.

--- XCode Project/SubOhmCalc/VV_VW_ViewController.h
+++ XCode Project/SubOhmCalc/VV_VW_ViewController.h
@@ -8,6 +8,38 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface VV_VW_ViewController : NSViewController
+@interface VV_VW_ViewController : NSViewController{
+    float voltage, resistance, wattage, power;
+}
+
+typedef enum changeType{
+    wattageChanged = 1,
+    voltageChanged,
+    resistanceChanged
+}changeType;
+
+@property (weak) IBOutlet NSSlider *voltageSlider;
+@property (weak) IBOutlet NSSlider *resistanceSlider;
+@property (weak) IBOutlet NSSlider *wattageSlider;
+
+@property (weak) IBOutlet NSTextField *voltageLabel;
+@property (weak) IBOutlet NSTextField *wattageLabel;
+@property (weak) IBOutlet NSTextField *resistanceLabel;
+
+
+- (IBAction)voltageSliderDidChange:(NSSlider *)sender;
+- (IBAction)resistanceSliderDidChange:(NSSlider *)sender;
+- (IBAction)wattageSliderDidChange:(id)sender;
+
+
+
+
+
+
+
+
+
+
+
 
 @end
--- XCode Project/SubOhmCalc/VV_VW_ViewController.m
+++ XCode Project/SubOhmCalc/VV_VW_ViewController.m
@@ -17,6 +17,74 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do view setup here.
+    
+    // set initial values...
+    resistance = 1.5;
+    voltage = 4.2;
+    power = voltage / resistance; // R = V / I
+    wattage = (voltage * voltage) / resistance;
+    
+    [self updateSlidersAndLabels];
 }
 
+- (IBAction)voltageSliderDidChange:(NSSlider *)sender {
+    voltage = self.voltageSlider.floatValue;
+    
+    [self calculateValues:voltageChanged];
+}
+
+- (IBAction)resistanceSliderDidChange:(NSSlider *)sender {
+    resistance = self.resistanceSlider.floatValue / 10;
+    
+    [self calculateValues:resistanceChanged];
+}
+
+- (IBAction)wattageSliderDidChange:(id)sender {
+    
+    wattage = self.wattageSlider.floatValue;
+    
+    [self calculateValues:wattageChanged];
+}
+
+-(void)calculateValues:(changeType)typeOfChange{
+    
+    switch (typeOfChange) {
+        case voltageChanged:
+            // calculate
+            wattage = (voltage * voltage) / resistance;
+            power = wattage / voltage;
+            [self updateSlidersAndLabels];
+            
+            break;
+            
+        case resistanceChanged:
+            // calculate
+            wattage = (voltage * voltage) / resistance;
+            voltage = wattage / power;
+            [self updateSlidersAndLabels];
+            
+            break;
+            
+        case wattageChanged:
+            // calculate
+            voltage = wattage / power;
+            [self updateSlidersAndLabels];
+            
+            break;
+            
+        default:
+            break;
+    }
+}
+
+-(void)updateSlidersAndLabels{
+    
+    self.wattageLabel.stringValue = [NSString stringWithFormat:@"%.2f W", wattage];
+    self.voltageLabel.stringValue = [NSString stringWithFormat:@"%.2f V", voltage];
+    self.resistanceLabel.stringValue = [NSString stringWithFormat:@"%.2f Ω", resistance];
+    
+    [self.wattageSlider setFloatValue:wattage];
+    [self.voltageSlider setFloatValue:voltage];
+    [self.resistanceSlider setFloatValue:resistance *10];
+}
 @end
